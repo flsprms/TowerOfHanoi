c38f00ebbb6c48a31b36f0027cc58df14b3f0a52
flsprms <149524130+flsprms@users.noreply.github.com>
2023-10-31 21:00:27 +0300

    Initial commit

---
+++ TowerOfHanoi/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace TowerOfHanoi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.takeButton1 = new System.Windows.Forms.Button();
+            this.takeButton2 = new System.Windows.Forms.Button();
+            this.takeButton3 = new System.Windows.Forms.Button();
+            this.putButton1 = new System.Windows.Forms.Button();
+            this.putButton2 = new System.Windows.Forms.Button();
+            this.putButton3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // takeButton1
+            // 
+            this.takeButton1.Location = new System.Drawing.Point(35, 360);
+            this.takeButton1.Name = "takeButton1";
+            this.takeButton1.Size = new System.Drawing.Size(100, 50);
+            this.takeButton1.TabIndex = 4;
+            this.takeButton1.Text = "Take";
+            this.takeButton1.UseVisualStyleBackColor = true;
+            this.takeButton1.Visible = false;
+            this.takeButton1.Click += new System.EventHandler(this.takeButtons_Click);
+            // 
+            // takeButton2
+            // 
+            this.takeButton2.Location = new System.Drawing.Point(205, 360);
+            this.takeButton2.Name = "takeButton2";
+            this.takeButton2.Size = new System.Drawing.Size(100, 50);
+            this.takeButton2.TabIndex = 5;
+            this.takeButton2.Text = "Take";
+            this.takeButton2.UseVisualStyleBackColor = true;
+            this.takeButton2.Visible = false;
+            this.takeButton2.Click += new System.EventHandler(this.takeButtons_Click);
+            // 
+            // takeButton3
+            // 
+            this.takeButton3.Location = new System.Drawing.Point(375, 360);
+            this.takeButton3.Name = "takeButton3";
+            this.takeButton3.Size = new System.Drawing.Size(100, 50);
+            this.takeButton3.TabIndex = 6;
+            this.takeButton3.Text = "Take";
+            this.takeButton3.UseVisualStyleBackColor = true;
+            this.takeButton3.Visible = false;
+            this.takeButton3.Click += new System.EventHandler(this.takeButtons_Click);
+            // 
+            // putButton1
+            // 
+            this.putButton1.Location = new System.Drawing.Point(35, 360);
+            this.putButton1.Name = "putButton1";
+            this.putButton1.Size = new System.Drawing.Size(100, 50);
+            this.putButton1.TabIndex = 7;
+            this.putButton1.Text = "Put";
+            this.putButton1.UseVisualStyleBackColor = true;
+            this.putButton1.Visible = false;
+            this.putButton1.Click += new System.EventHandler(this.putButtons_Click);
+            // 
+            // putButton2
+            // 
+            this.putButton2.Location = new System.Drawing.Point(205, 360);
+            this.putButton2.Name = "putButton2";
+            this.putButton2.Size = new System.Drawing.Size(100, 50);
+            this.putButton2.TabIndex = 8;
+            this.putButton2.Text = "Put";
+            this.putButton2.UseVisualStyleBackColor = true;
+            this.putButton2.Visible = false;
+            this.putButton2.Click += new System.EventHandler(this.putButtons_Click);
+            // 
+            // putButton3
+            // 
+            this.putButton3.Location = new System.Drawing.Point(375, 360);
+            this.putButton3.Name = "putButton3";
+            this.putButton3.Size = new System.Drawing.Size(100, 50);
+            this.putButton3.TabIndex = 9;
+            this.putButton3.Text = "Put";
+            this.putButton3.UseVisualStyleBackColor = true;
+            this.putButton3.Visible = false;
+            this.putButton3.Click += new System.EventHandler(this.putButtons_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(175, 451);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 31);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Шагов: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 491);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.putButton3);
+            this.Controls.Add(this.putButton2);
+            this.Controls.Add(this.putButton1);
+            this.Controls.Add(this.takeButton3);
+            this.Controls.Add(this.takeButton2);
+            this.Controls.Add(this.takeButton1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ханойские башни";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button takeButton1;
+        private System.Windows.Forms.Button takeButton2;
+        private System.Windows.Forms.Button takeButton3;
+        private System.Windows.Forms.Button putButton1;
+        private System.Windows.Forms.Button putButton2;
+        private System.Windows.Forms.Button putButton3;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TowerOfHanoi/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TowerOfHanoi
+{
+    public partial class Form1 : Form
+    {
+        Random rnd = new Random();
+
+        public static List<Button> takeButtons;
+        public static List<Button> putButtons;
+        public static List<Tower> towers;
+
+        //временное хранилище для кольца, взятого методом Take
+        public static PictureBox temp;
+
+        public static int steps;
+
+        public class Tower
+        {
+            public int placeIndex = 0;
+            public Point placeCenterPoint;
+            public List<PictureBox> rings = new List<PictureBox>();
+            public Tower(int place)
+            {
+                this.placeIndex = place;
+                this.placeCenterPoint = new Point(85 + placeIndex * 170, 282);
+            }
+            public void Paint()
+            {
+                int j = 0;
+
+
+                foreach(PictureBox ring in rings)
+                {
+                    int i = ring.Size.Width / 50;
+
+                    int x = placeCenterPoint.X - 25 * i;
+                    int y = placeCenterPoint.Y - 58 * j;
+
+                    ring.Location = new Point(x, y);
+                    j++;
+                }
+            }
+            public void Take()
+            {
+                PictureBox takedRing = rings.Last();
+                rings.RemoveAt(rings.Count-1);
+                takedRing.Top = 100;
+
+                temp = takedRing;
+
+                CheckTowersToPut();
+            }
+        }
+
+        //проверка башен на возможность взять кольцо
+        public static void CheckTowersToTake()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (towers[i].rings.Count > 0)
+                {
+                    takeButtons[i].Visible = true;
+                }
+            }
+        }
+
+        //проверка башен на возможность поместить туда кольцо
+        public static void CheckTowersToPut()
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                if (towers[i].rings.Count == 0 || temp.Width < towers[i].rings.Last().Width)
+                {
+                    putButtons[i].Visible = true;
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            takeButtons = new List<Button>() { takeButton1, takeButton2, takeButton3 };
+            putButtons = new List<Button>() { putButton1, putButton2, putButton3 };
+            towers = new List<Tower>() { new Tower(0), new Tower(1), new Tower(2) };
+
+            //добавление колец на первую башню при инициализации формы
+            for (int i = 3; i > 0; i--)
+            {
+                PictureBox ring = new PictureBox();
+                ring.Name = i.ToString();
+
+                ring.BackColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+                ring.Size = new Size(50 * i, 50);
+                ring.Visible = true;
+                ring.Enabled = true;
+
+                this.Controls.Add(ring);
+                towers[0].rings.Add(ring);
+            }
+
+            CheckTowersToTake();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //отрисовка стеднов для башен
+            Graphics g;
+            g = CreateGraphics();
+            g.DrawLine(new Pen(Color.Black, 10), 85, 155, 85, 340);
+            g.DrawLine(new Pen(Color.Black, 10), 255, 155, 255, 340);
+            g.DrawLine(new Pen(Color.Black, 10), 425, 155, 425, 340);
+
+            //отрисовка колец на каждой башне
+            foreach(Tower tower in towers)
+            {
+                tower.Paint();
+            }
+
+            label1.Text = "Шагов: " + steps;
+        }
+
+        private void takeButtons_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            int index = takeButtons.FindIndex(b => b == button);
+
+            towers[index].Take();
+
+            foreach (Button takeButton in takeButtons)
+            {
+                takeButton.Visible = false;
+            }
+
+            this.Refresh();
+        }
+
+        private void putButtons_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            int index = putButtons.FindIndex(b => b == button);
+
+            towers[index].rings.Add(temp);
+
+            foreach(Button putButton in putButtons)
+            {
+                putButton.Visible = false;
+            }
+
+            CheckTowersToTake();
+
+            this.Refresh();
+
+            steps++;
+
+            if (towers[2].rings.Count == 3)
+            {
+                MessageBox.Show("Вы прошли головоломку 'Ханойские башни'!", "Поздравляем!");
+                for (int i = 0; i < 3; i++)
+                {
+                    takeButtons[i].Visible = false;
+                    putButtons[i].Visible = false;
+                }
+
+            }
+        }
+    }
+}
